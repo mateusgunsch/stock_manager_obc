ee96af61c28147572502c2188742eb0382b05e6f
Mateus Gunsch <mateusgunsch01@gmail.com>
2024-11-21 14:16:43 -0400

    Context for stock items created
    Class StockItem created to save items
    
    Table of items included to list all items in stock
    
    Fomr to add new items to stock and all logic added

---
+++ src/components/ItemForm.jsx
@@ -0,0 +1,124 @@
+import { useContext, useRef, useState } from "react"
+import PropTypes from "prop-types"
+import StockItem, { CATEGORIES } from "../entities/StockItem"
+import { StockContext } from "../contexts/ConstStockContext"
+
+ItemForm.propTypes = {
+    itemToUpdate: PropTypes.object
+}
+
+export default function ItemForm({ itemToUpdate }) {
+    const defaultItem = {
+        name: "",
+        description: "",
+        quantity: 0,
+        price: 0,
+        category: ""
+    }
+    const [item, setItem] = useState(itemToUpdate ? itemToUpdate : defaultItem)
+    const { addItem } = useContext(StockContext)
+    const inputRef = useRef(null)
+
+    const handleChange = (ev) => {
+        setItem(state => (
+            {
+                ...state,
+                [ev.target.name]: ev.target.value
+            }
+        ))
+    }
+
+    const handleSubmit = (ev) => {
+        ev.preventDefault()
+
+        try {
+            const stockItem = new StockItem(item)
+            addItem(stockItem)
+            setItem(defaultItem)
+            alert("Item cadastrado com sucesso!")
+            inputRef.current.focus()
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="row">
+                <div>
+                    <label htmlFor="name">Nome</label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        required
+                        ref={inputRef}
+                        value={item.name}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="quantity">Quantidade</label>
+                    <input
+                        type="number"
+                        name="quantity"
+                        id="quantity"
+                        required
+                        min={0}
+                        step={1}
+                        value={item.quantity}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="price">Preço</label>
+                    <input
+                        type="number"
+                        name="price"
+                        id="price"
+                        required
+                        min={0.00}
+                        step={0.01}
+                        value={item.price}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="category">Categoria</label>
+                    <select
+                        name="category"
+                        id="category"
+                        required
+                        value={item.category}
+                        onChange={handleChange}
+                        >
+                        <option disabled value="">Selecione uma categoria...</option>
+                        {CATEGORIES.map((category) => (
+                            <option
+                                key={category}
+                                value={category}
+                                defaultChecked={item.category === category}
+                                >
+                                {category}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+            </div>
+            <div className="form-control">
+                <label htmlFor="description">Descrição</label>
+                <textarea
+                    name="description"
+                    id="description"
+                    required
+                    rows={6}
+                    value={item.description}
+                    onChange={handleChange}
+                ></textarea>
+            </div>
+            <button className="button is-primary is-large">
+                Salvar
+            </button>
+        </form>
+    )
+}
