aa298cc747b69408bc5346a56278cc58becbe122
Mateus Gunsch <mateusgunsch01@gmail.com>
2024-11-21 15:08:27 -0400

    Show item page and logic

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types"
 import { useContext } from "react"
 import { StockContext } from "../contexts/ConstStockContext"
+import { useNavigate } from "react-router-dom"
 
 DeleteButton.propTypes = {
     itemId: PropTypes.number,
@@ -9,16 +10,18 @@ DeleteButton.propTypes = {
 
 export default function DeleteButton({ itemId, itemName }) {
     const { deleteItem } = useContext(StockContext)
+    const navigate = useNavigate()
 
     const handleDelete = () => {
         if (confirm(`Deseja excluir ${itemName}?`)) {
             deleteItem(itemId)
+            navigate("/items")
         }
     }
 
     return (
         <button className="button is-danger is-small" onClick={handleDelete}>
-            <img src="trash.svg" alt="Excluir" />
+            <img src="/trash.svg" alt="Excluir" />
         </button>
     )
 }
--- src/pages/items/ShowItem.jsx
+++ src/pages/items/ShowItem.jsx
@@ -1,5 +1,29 @@
+import { useContext } from "react"
+import { Link, useParams } from "react-router-dom"
+import { StockContext } from "../../contexts/ConstStockContext"
+import DeleteButton from "../../components/DeleteButton"
+
 export default function ShowItem() {
+    const { getItem } = useContext(StockContext)
+    const { id } = useParams()
+
+    const item = getItem(id)
+
     return(
-        <h2>Exibir Item</h2>
+        <div className="item">
+            <h2>{item.name}</h2>
+            <Link to={`/items/${item.id}/update`} className="button is-small">Atualizar</Link>
+            <DeleteButton itemId={item.id} itemName={item.name} />
+            <div className="row">
+                <span>Categoria: {item.category}</span>
+                <span>Quantidade em estoque: {item.quantity}</span>
+                <span>Preço: R$ {item.price}</span>
+            </div>
+            <p>{item.description}</p>
+            <div className="row">
+                <p>Cadastrado em: {item.createdAt.toLocaleDateString()}</p>
+                <p>Atualizado em: {item.updatedAt.toLocaleDateString()}</p>
+            </div>
+        </div>
     )
 }
